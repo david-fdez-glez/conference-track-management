7fe6ed8523b0d2b544faf04141d647d4e9ea8ecd
David Fernández González <fernandezgonzalez.d@gmail.com>
2016-05-02 00:05:03 +0100

    Add Main Class functionality

--- src/main/java/org/dfernandez/ConferenceManagerMain.java
+++ src/main/java/org/dfernandez/ConferenceManagerMain.java
@@ -1,6 +1,12 @@
 package org.dfernandez;
 
 
+import org.dfernandez.model.Conference;
+import org.dfernandez.service.ConferenceScheduler;
+import org.dfernandez.util.FilesUtil;
+
+import java.util.List;
+
 public class ConferenceManagerMain
 {
     public static void main( String[] args )
@@ -9,6 +15,14 @@ public class ConferenceManagerMain
 
         if( args.length > 0) {
             path = args[0];
+        } else {
+            System.out.println(" You didn't provide a file path, the system will use " + path + "\n") ;
         }
+
+        List<String> lines = FilesUtil.readAllLines(path);
+        Conference conference = new ConferenceScheduler().schedule(lines);
+
+        System.out.println(conference);
+        System.out.println();
     }
 }
